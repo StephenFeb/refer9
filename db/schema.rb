a69c78b7f7855d194a53d45010d5c532467b7a07
StephenFeb <spinshon@hotmail.com>
2020-09-23 22:17:28 +0100

    creating profiles page

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_19_204153) do
+ActiveRecord::Schema.define(version: 2020_09_23_183048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.string "job_title"
+    t.text "work_experience"
+    t.text "skills"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
