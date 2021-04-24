48d956cd89d9ef3cb58b2263c2e02f79d122744e
StephenFeb <spinshon@hotmail.com>
2021-04-24 17:18:31 +0100

    fixed a bit of the frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating fake profiles"
+
+Profile.create(name: "John Smith", job_title: "Sales Executive", work_experience: "5 years sales experience", skills: "Sales, Marketing")
+
+Profile.create(name: "Mark Davies", job_title: "FInancial Advisor", work_experience: "5 years financial advice", skills: "Financial Advice")
+
+Profile.create(name: "Rachel Green", job_title: "Fashion Executive", work_experience: "5 years fashion marketing", skills: "Sales, Marketing")
+
+Profile.create(name: "Chandler Bing", job_title: "Executive", work_experience: "5 years excel", skills: "Excel, CRM Systems")
+
+puts "4 fake profiles created..."
