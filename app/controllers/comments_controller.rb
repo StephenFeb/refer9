063756e451427734b65674ff9a28880395fc3123
StephenFeb <spinshon@hotmail.com>
2021-05-13 19:11:14 +0100

    added comments functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,42 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_comment, only: [:show, :destroy]
+
+
+def index
+  @comment = Comment.all
+end
+
+def new
+  @post = Post.find(params[:post_id])
+  @comment = Comment.new
+end
+
+def create
+    @comment = Comment.new(comments_params)
+    @post = Post.find(params[:post_id])
+    @comment.user_id = current_user
+    @comment.post = @post
+
+          if @comment.save
+            redirect_to post_path
+          else
+            render :new
+          end
+    
+    
+end
+
+private
+
+def comments_params
+    params.require(:comment).permit(:content)
+end
+
+def set_comment
+  @comment = Comment.find(params[:id])
+end
+
+
+
+end
