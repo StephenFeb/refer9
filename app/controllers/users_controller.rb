2a47edc362a34872c1bea3872179b2dd905f5ef3
StephenFeb <spinshon@hotmail.com>
2021-07-15 23:06:58 +0100

    designing user profiles page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,24 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
-    before_action :set_user, only: [:show]
+    # before_action :set_user, only: [:show]
 
 
 def index
     # user profiles feed
+    @users = User.all
 end
 
 def show
     # user profile
+    @user = User.find(params[:id])
 end
 
 
 private
 
-def set_user
-  @user = User.find(params[:username])
-end
+# def set_user
+  # @user = User.find(params[:username])
+# end
 
 def user_params
     params.require(:user).permit(:first_name, :last_name, :bio, :cv)
